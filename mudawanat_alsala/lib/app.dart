b70e3cb07af7f77b9ebbd2683f5fcfc8f0d5562d
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-01 08:22:22 +0200

    ....

---
+++ mudawanat_alsala/lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
