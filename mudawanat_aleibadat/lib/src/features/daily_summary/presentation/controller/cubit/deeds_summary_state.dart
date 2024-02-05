656b2ac95135450dac99c0f9722a7f3202319d81
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-05 03:16:32 +0200

    Update deeds_summary_state.dart

--- mudawanat_aleibadat/lib/src/features/daily_summary/presentation/controller/cubit/deeds_summary_state.dart
+++ mudawanat_aleibadat/lib/src/features/daily_summary/presentation/controller/cubit/deeds_summary_state.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'deeds_summary_cubit.dart';
 
 sealed class DeedsSummaryState extends Equatable {
@@ -33,5 +34,21 @@ final class DeedsSummaryLoaded extends DeedsSummaryState {
       awradElements,
       fastingElement,
     ];
+  }
+
+  DeedsSummaryLoaded copyWith({
+    int? totalDays,
+    List<StatsElement>? obligatoryElements,
+    List<StatsElement>? additionalElements,
+    List<StatsElement>? awradElements,
+    StatsElement? fastingElement,
+  }) {
+    return DeedsSummaryLoaded(
+      totalDays: totalDays ?? this.totalDays,
+      obligatoryElements: obligatoryElements ?? this.obligatoryElements,
+      additionalElements: additionalElements ?? this.additionalElements,
+      awradElements: awradElements ?? this.awradElements,
+      fastingElement: fastingElement ?? this.fastingElement,
+    );
   }
 }
