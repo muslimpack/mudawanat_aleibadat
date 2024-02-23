3c5680ff48206c62427e17d8db4fc37107af010b
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-23 20:40:06 +0200

    Create deeds_switch_tile.dart

---
+++ mudawanat_aleibadat/lib/src/features/daily_deeds/presentation/components/deeds_switch_tile.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:mudawanat_aleibadat/src/core/utils/color_generator.dart';
+
+class DeedsSwitchTile extends StatelessWidget {
+  final bool value;
+  final Widget? title;
+  final ValueChanged<bool>? onChanged;
+  final Widget? info;
+  const DeedsSwitchTile({
+    super.key,
+    required this.value,
+    this.title,
+    this.onChanged,
+    this.info,
+  });
+  String getKey() {
+    if (title != null && title is Text) {
+      final text = (title! as Text).data;
+      final key = text ?? title.toString();
+
+      return key + key;
+    }
+    return title.hashCode.toString();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final color = ColorGenerator.highContrastByKey(
+      key: getKey(),
+    );
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      color: color.withOpacity(.1),
+      child: Column(
+        children: [
+          SwitchListTile(
+            value: value,
+            title: title,
+            onChanged: onChanged,
+          ),
+          if (info != null) info!,
+        ],
+      ),
+    );
+  }
+}
